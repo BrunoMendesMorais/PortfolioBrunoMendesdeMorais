ce329b57d4673690651004adbb32677f70f8c907
BrunoMendesMorais <mendesdemoraisbruno@gmail.com>
2025-06-28 12:34:52 -0300

    ajustes na home 🔵

--- app/Views/main/Home.php
+++ app/Views/main/Home.php
@@ -9,21 +9,22 @@
 <?= $this->section('conteudo') ?>
 
 <main>
-    <section id="presentacao">
-        <h1>DESENVOLVEDOR</h1>
-        <h2>FULL-STACK</h2>
-        <img src="imagensSite/Bruno.jpg" alt="">
-    </section>
-
-    <section id="sobreMim">
-        <h3>SOBRE MIM</h3>
-        <p>Sou um desenvolvedor de software focado na area da web tenho habilidades tanto no frontend quanto no backend</p>
-        <section id="maisSobreMim">
-
+    <section id="inicial">
+        <section id="presentacao">
+            <h1>DESENVOLVEDOR</h1>
+            <h2>FULL-STACK</h2>
+            <img src="imagensSite/Bruno.jpg" alt="">
         </section>
-        <a id="verMaisSobreMim">VER MAIS</a>
-    </section>
 
+        <section id="sobreMim">
+            <h3>SOBRE MIM</h3>
+            <p>Sou um desenvolvedor de software focado na area da web tenho habilidades tanto no frontend quanto no backend</p>
+            <section id="maisSobreMim">
+
+            </section>
+            <a id="verMaisSobreMim">VER MAIS</a>
+        </section>
+    </section>
     <section id="projetos">
         <h3>PROJETOS</h3>
         <div>
