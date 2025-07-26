0d8f474152deaa0cf622f7d0e59d0e207029f5de
BrunoMendesMorais <mendesdemoraisbruno@gmail.com>
2025-07-26 13:27:41 -0300

    identificando tecnologias do projeto

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\ProjetoModel;
+use App\Models\ProjetoTecnologiaModel;
 use App\Models\TecnologiaModel;
 
 class Home extends BaseController
@@ -10,10 +11,25 @@ class Home extends BaseController
     public function index()
     {
         $dados = array();
+        $itens['tecnologia'] = array();
+        $a = array();
         $TecnologiaModel = new TecnologiaModel();
         $ProjetoModel = new ProjetoModel();
+        $ProjetoTecnologiaModel = new ProjetoTecnologiaModel();
         $dados["projeto"] = $ProjetoModel->select('titulo, img_capa, id_projeto',)->findAll();
-        $dados["tecnologia"] = $TecnologiaModel->select()->findAll();
+
+        foreach ($dados['projeto'] as $itens) {
+            $filtro = $ProjetoTecnologiaModel->where('projeto_id', $itens['id_projeto'])->find();
+            $b['tecnologias'] = array();
+            foreach ($filtro as $item) {
+                $selecionado = $TecnologiaModel->where('id_tecnologia',$item['tecnologia_id'])->select('img_tecnologia')->first();
+                array_push($b['tecnologias'], $selecionado);
+            }
+            $unido = array_merge($itens, $b);
+            array_push($a, $unido);
+        }
+        $dados["projeto"] = $a;
+        $dados["tecnologia"] = $TecnologiaModel->findAll();
         return view('main/Home', $dados);
     }
 }
--- app/Controllers/ProjetoController.php
+++ app/Controllers/ProjetoController.php
@@ -6,13 +6,19 @@ use App\Controllers\BaseController;
 use App\Models\ImagemModel;
 use App\Models\ImagemProjeto;
 use App\Models\ProjetoModel;
+use App\Models\TecnologiaModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class ProjetoController extends BaseController
 {
     public function index()
     {
-        return view("main/Projetos");
+        $dados = array();
+        $TecnologiaModel = new TecnologiaModel();
+        $ProjetoModel = new ProjetoModel();
+        $dados["projeto"] = $ProjetoModel->select('titulo, img_capa, id_projeto',)->findAll();
+        $dados["tecnologia"] = $TecnologiaModel->select()->findAll();
+        return view("main/Projetos", $dados);
     }
 
     public function DetalheDoProjeto($id)
@@ -26,11 +32,11 @@ class ProjetoController extends BaseController
         $a = $ImagemProjetoModel->where('projeto_id', $id)->findAll();
         $c = array();
 
-        foreach ($a as $tomate) {
-            $img = $ImagemModel->where('id_imagem', $tomate['imagem_id'])->first();
-            array_push($c,$img);
+        foreach ($a as $itens) {
+            $img = $ImagemModel->where('id_imagem', $itens['imagem_id'])->first();
+            array_push($c, $img);
         }
-        
+
         $dados['imagens'] = $c;
         $dados['projeto'] = $ProjetoModel->find($id);
         return view("main/DetalheDoProjeto", $dados);
