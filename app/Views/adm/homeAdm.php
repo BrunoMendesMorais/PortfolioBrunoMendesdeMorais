a86df967105df6686893aca466eed4ef5eb72414
BrunoMendesMorais <mendesdemoraisbruno@gmail.com>
2025-07-27 17:56:19 -0300

    ajustes 🔵

--- app/Views/adm/homeAdm.php
+++ app/Views/adm/homeAdm.php
@@ -1,1 +1,14 @@
 <?= $this->extend('partials/adm') ?>
+
+<?= $this->section('head') ?>
+
+<link rel="stylesheet" href="/css/adm/homeAdm.css">
+
+<?= $this->endsection(); ?>
+
+<?= $this->section('conteudo') ?>
+<main>
+    <h1></h1>
+</main>
+
+<?= $this->endsection(); ?>
--- app/Views/partials/adm.php
+++ app/Views/partials/adm.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/partials/adm.css">
+    <?= $this->renderSection('head') ?>
     <title>ADM</title>
 </head>
 
@@ -15,8 +16,8 @@
                 <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
             </svg>
         </a>
-        <p>ADM</p>
     </header>
+    <?= $this->renderSection('conteudo') ?>
 </body>
 
 </html>
