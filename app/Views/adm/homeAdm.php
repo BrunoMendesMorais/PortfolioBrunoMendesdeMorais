b864e5b76b64ad63a4146619ee5596d217a490d9
BrunoMendesMorais <mendesdemoraisbruno@gmail.com>
2025-07-28 19:49:37 -0300

    painel adm tecnologia 🟢

--- app/Views/adm/homeAdm.php
+++ app/Views/adm/homeAdm.php
@@ -8,7 +8,7 @@
 
 <?= $this->section('conteudo') ?>
 <main>
-      <h1>Home</h1>
+    <h1>Home</h1>
     <section id="home">
         <form id="formularioTexto" action="" method="post">
             <textarea name="resumo" id="resumo"></textarea>
@@ -21,13 +21,44 @@
             <input type="file" name="img" id="img">
         </form>
     </section>
+    <h1>Tecnologia</h1>
+    <section id="tecnologia">
+        <section id="tec">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+            <img src="/img/tecnologias/ts.svg" alt="">
+        </section>
+
+        <form id="novaTec">
+            <label for="imgTec"><img src="/img/imagensSite/plus.svg" alt=""></label>
+            <inpt type="file" name="imgTec" id="imgTec">
+            <input type="text" name="nomeTec" id="nomeTec">
+            <button>Finalizar</button>
+        </form>
+
+    </section>
 </main>
+
 <script>
     document.addEventListener("input", function(event) {
         if (event.target.tagName.toLowerCase() !== "textarea") return;
 
-        event.target.style.height = "auto"; // Reset the height
-        event.target.style.height = (event.target.scrollHeight) + "px"; // Set it to scroll height
+        event.target.style.height = "auto";
+        event.target.style.height = (event.target.scrollHeight) + "px";
     });
 </script>
 
